a4d456d88fda3e2a842d6b27bc869ae2963eb9ba
Daniel Chang <daniel.chang@cloudnifier.com>
2023-02-13 14:36:06 +0800

    Iteration03

--- BowlingGame/Game.cs
+++ BowlingGame/Game.cs
@@ -2,16 +2,43 @@
 {
     public class Game
     {
-        private int totalScore;
+        private int[] rolls = new int[20];
+        private int currentRoll = 0;
 
         public void Roll(int pins) // 投球
         {
-            totalScore += pins;
+            rolls[currentRoll] = pins;
+            currentRoll++;
         }
 
         public int Score() // 計分
         {
-            return totalScore;
+            int totalSocre = 0;
+            int frameIndex = 0;
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (isSpare(frameIndex)) // Spare
+                {
+                    totalSocre += (rolls[frameIndex] + rolls[frameIndex + 1] + rolls[frameIndex + 2]);
+                    frameIndex += 2;
+                }
+                else
+                {
+                    totalSocre += rolls[frameIndex] + rolls[frameIndex + 1];
+                    frameIndex += 2;
+                }
+            }
+            return totalSocre;
+        }
+
+        /// <summary>
+        /// 判斷是否為補中(Spare)
+        /// </summary>
+        /// <param name="frameIndex">目前局數</param>
+        /// <returns>True 代表補中，反之為 False</returns>
+        private bool isSpare(int frameIndex)
+        {
+            return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
         }
     }
 }
--- BowlingGameTest/GameTest.cs
+++ BowlingGameTest/GameTest.cs
@@ -22,8 +22,8 @@ namespace BowlingGameTest
             // 3A Patten
 
             // Arrange
-            int expected = 0;
-            int actual;
+            int expected = 0; // 期望結果值
+            int actual; // 實際結果值
 
             // Act
             RollMany(20, 0);
@@ -34,18 +34,45 @@ namespace BowlingGameTest
         }
 
         /// <summary>
-        /// 測試玩家投球 20 次，每次都只得 1 分
+        /// 測試玩家投球 20 次，每次都只得 1 分時的總得分
         /// </summary>
         [TestMethod]
         public void TestAllOnes()
         {
             int expected = 20;
-            int actual = 0;
+            int actual;
 
             RollMany(20, 1);
             actual = g.Score();
 
             Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// 測試玩家整局只有 1 次補中(Spare)的總得分(共 20 次投球機會，其餘 17 次皆 0 分)
+        /// 1st 5/5(Spare)，2nd 3/0，其餘皆洗溝
+        /// 5 + 5 = 10 (1st)
+        /// 10 + 3 = 13 (Spare Bouns)
+        /// 13 + 3 + 0 = 16 (2nd)
+        /// </summary>
+        [TestMethod]
+        public void TestOneSpare()
+        {
+            int expected = 16;
+            int actual;
+
+            RollSpare();
+            g.Roll(3);
+            RollMany(17, 0);
+            actual = g.Score();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        private void RollSpare()
+        {
+            g.Roll(5);
+            g.Roll(5);
         }
 
         private void RollMany(int times, int pins)
